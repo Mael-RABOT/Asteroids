c111e9ea5bdbf4e558b3a0284af5ceb1feea358b
Mael-RABOT <mael.rabot@epitech.eu>
2024-04-27 03:49:28 +0200

    fix(code): add overload assert
    Signed-off-by: Mael-RABOT <mael.rabot@epitech.eu>

--- Cobra/src/main.py
+++ Cobra/src/main.py
@@ -1,4 +1,8 @@
 import Wrapper
+from Vector2D import Vector2D
+
+def is_str_overloaded(cls: type) -> bool:
+    return cls.__str__ is not object.__str__
 
 
 def main() -> None:
@@ -6,6 +10,7 @@ def main() -> None:
     Entry point of the program.
     :return:
     """
+    assert is_str_overloaded(Vector2D), "Le vecteur n'est pas encore réparé"
     wrapper = Wrapper.Wrapper(1920, 1080)
     wrapper.run()
 
